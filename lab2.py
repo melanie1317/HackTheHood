c935cbb4c99c083a6fe12db487db1505a6d91d81
Melanie Murillo <melaniemurillo@Mac.attlocal.net>
2025-06-18 17:36:37 -0700

    final lab2

---
+++ lab2.py
@@ -0,0 +1,14 @@
+# Lab 2: Melanie Murillo
+# Step 1: Name Variable
+name = "Melanie Murillo"
+# Step 2: Nickname Variable
+nickname = "Melanie"
+# Step 3: Age Variable
+# integer = whole num, float = decimal
+age = 17
+# Step 4: Boolean Variable (True or False)
+used_before = True
+# Step 5: List Data Strucutre
+hobbies = ["paint", "film photo", "TV", "animals"]
+# Step 6: Dictionary (key : value) pairs
+my_favs = {"food":["sushi", "tacos", "pizza", "boba"], "soda":"diet coke", "animal":"dog"}
